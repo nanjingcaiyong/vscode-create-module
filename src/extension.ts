a57af5fd3d6072cbc987988669bd25ddd4e3591f
caiyong <yong.cai@kapeixi.com>
2023-11-12 20:43:14 +0800

    feat: 读取用户配置

--- src/extension.ts
+++ src/extension.ts
@@ -2,23 +2,6 @@ import path = require('path');
 import * as vscode from 'vscode';
 import fs = require('fs');
 
-const renderVueTemplate = (pageName: string) => `<template>
-  <div>hello world</div>
-</template>
-<script>
-import { defineComponent } from 'vue';
-export default defineComponent({
-  name: '${pageName}'
-});
-</script>
-`;
-const renderEntryTemplate = (pageName: string) => `import App from './app.vue';
-import { createApp } from 'vue';
-import 'tailwindcss/tailwind.css';
-createApp(App)
-  .mount('#${pageName}');
-`;
-
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('rich.openRoot', async (url) => {
 		const pageName = await vscode.window.showInputBox({
@@ -33,38 +16,30 @@ export function activate(context: vscode.ExtensionContext) {
 			return;
 		}
 
-		//#region 
 		// 当前工作目录
-		// const currentWorkDir = (vscode.workspace.workspaceFolders|| [])[0].uri.path;
-		// const templatePath = path.resolve(currentWorkDir, 'mpa.template.js');
+		const currentWorkDir = (vscode.workspace.workspaceFolders|| [])[0].uri.path;
+		const templatePath = path.resolve(currentWorkDir, 'mpa.template.js');
 
 		// 如果工作根目录下没有map.template.js文件提示报错
-		// try {
-		// 	await fs.promises.stat(templatePath);
-		// } catch (err) {
-		// 	vscode.window.showErrorMessage('The template file "mpa.template.js" cannot be empty');
-		// 	return;
-		// }
-
-		// const filePath = path.normalize(path.resolve(url.path, pageName));
-		// if (!fs.existsSync(filePath)) {
-		// 	fs.mkdirSync(filePath);
-		// }
-
-		// 获取当前工作目录下mpa.tempalte.js中的模板配置
-		// const config = require(templatePath);
-		// const templates = config.render(pageName);
-		// Object.keys(templates).forEach(fileName => {
-		// 	fs.writeFile(filePath + `/${fileName}`, templates[fileName], () => {});
-		// });
-		//#endregion
+		try {
+			await fs.promises.stat(templatePath);
+		} catch (err) {
+			vscode.window.showErrorMessage('The template file "mpa.template.js" cannot be empty');
+			return;
+		}
 
 		const filePath = path.normalize(path.resolve(url.path, pageName));
 		if (!fs.existsSync(filePath)) {
 			fs.mkdirSync(filePath);
 		}
-		fs.writeFile(filePath + `/app.vue`, renderVueTemplate(pageName), () => {});
-		fs.writeFile(filePath + `/main.ts`, renderEntryTemplate(pageName), () => {});
+
+		// 获取当前工作目录下mpa.tempalte.js中的模板配置
+		const config = require(templatePath);
+
+		const templates = config.render(pageName);
+		Object.keys(templates).forEach(fileName => {
+			fs.writeFile(filePath + `/${fileName}`, templates[fileName], () => {});
+		});
 	});
 
 	context.subscriptions.push(disposable);
